f5fff79698485beaf53c6726f02dda930fbe4fc2
Timur Cravtov <timurcravtov04@gmail.com>
2025-09-09 11:28:58 +0300

    Add Image service

---
+++ server/src/main/java/utm/server/features/image/ImageController.java
@@ -0,0 +1,24 @@
+package utm.server.features.image;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+        String link = imageService.upload(file);
+        return ResponseEntity.ok(link);
+    }
+
+}

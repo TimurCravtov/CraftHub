0be7d6339ed16b72dfb27756c70c631257919ca3
arturtugui <146761295+arturtugui@users.noreply.github.com>
2025-09-15 11:32:53 +0300

    Added the cart functionality from another branch (I leaked the api keys there, so I added the hard way)

---
+++ server/src/main/java/utm/server/features/cart/dto/CartItemResponse.java
@@ -0,0 +1,32 @@
+package utm.server.features.cart.dto;
+
+public class CartItemResponse {
+    private Long productId;
+    private String productName;
+    private int quantity;
+
+    // Getters and setters
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
